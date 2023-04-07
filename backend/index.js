9895e6487c02f1d8b3119d3abca6abd0fa2d7490
Antonius Wisnu <antonius.kri2003@mail.ugm.ac.id>
2023-04-08 03:05:29 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/fullstack_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(5000, () => console.log("Server started"));
